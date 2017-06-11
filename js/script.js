b4d313999e4b30998cb0eb4d0a98a5b7cc3ac524
Van Roye <steven.van-roye@sogeti.com>
2017-06-11 15:38:52 +0200

    Use of MVO model

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ dogPictureRight.addEventListener('click',function(){
 })
 */
 
-/* PART WITH LIST : LIST OF DOGS SEE COMMIT OF 10 JUNE 2017 'CHANGE TO LIST OF DOGS INSTEAD OF 2 DOGS'*/
+/* PART WITH LIST : LIST OF DOGS SEE COMMIT OF 10 JUNE 2017 'CHANGE TO LIST OF DOGS INSTEAD OF 2 DOGS'
 var numberOfItems = 3;
 var dogArray = [];
 
@@ -64,3 +64,120 @@ for (var i = 0; i < numberOfItems; i++) {
 	})(currentElement));
 
 };
+
+
+*/
+
+var $sideList = $('#list');
+var $art= $('article');
+var $imgContainer = $('.img-container');
+var $timesClicked = $('.times-clicked');
+var $dogName = $('.click-me');
+var $imgDog = $('.dog-picture');
+
+
+var model = {
+	currentDog: null,
+	dogs: [
+		{
+			imageName: "images/fenji1.jpg",
+			dogName: "Fenji 1",
+			score: 0
+		},
+		{
+			imageName: "images/fenji2.jpg",
+			dogName: "Fenji 2",
+			score: 0
+		},
+		{
+			imageName: "images/fenji3.jpg",
+			dogName: "Fenji 3",
+			score: 0
+		}
+	]
+};
+
+var viewClicker = {
+	init: function(){
+		this.dogElement = $('article');
+		this.dogNameElement = document.getElementById('clickMe');
+		this.dogImageElement = document.getElementById('dogPicture');
+		this.scoreElement = document.getElementById('timesClicked');
+
+		this.dogImageElement.addEventListener('click', function(){
+			octopus.incrementCounter();
+		});
+
+		this.render();
+	},
+	render: function(){
+		var currentDog = octopus.getCurrentDog();
+		this.scoreElement.textContent = currentDog.score;
+		this.dogNameElement.textContent = currentDog.dogName;
+		this.dogImageElement.src = currentDog.imageName;
+	}
+};
+
+var viewList = {
+	init: function(){
+		this.dogList = document.getElementById('list');
+		this.render();
+	},
+	render: function(){
+		var dog,elem;
+		var dogs = octopus.getDogs();
+		this.dogList.innerHTML = '';
+
+		for (i = 0; i < dogs.length; i++) {
+            // this is the cat we're currently looping over
+            dog = dogs[i];
+
+            // make a new cat list item and set its text
+            elem = document.createElement('label');
+            elem.innerHTML = '<input type="radio" name="dogName" class="list-item" value="dog">' + dog.dogName + '<br>';
+            //elem.setAttribute('type','radio');
+            //elem.setAttribute('name','dogname');
+            //elem.setAttribute('class','list-item');
+            //elem.setAttribute('value','dog');
+
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
+            elem.addEventListener('click', (function(dogCopy) {
+                return function() {
+                    octopus.setCurrentDog(dogCopy);
+                    viewClicker.render();
+                };
+            })(dog));
+
+            // finally, add the element to the list
+            this.dogList.appendChild(elem);
+
+        }
+
+	}
+};
+
+var octopus = {
+	init: function(){
+		model.currentDog = model.dogs[0];
+		viewList.init();
+		viewClicker.init();
+	},
+	getCurrentDog: function(){
+		return model.currentDog;
+	},
+	getDogs: function(){
+		return model.dogs;
+	},
+	setCurrentDog: function(dog){
+		model.currentDog = dog;
+	},
+	incrementCounter: function(){
+		model.currentDog.score++;
+		viewClicker.render();
+	}
+};
+
+octopus.init();
